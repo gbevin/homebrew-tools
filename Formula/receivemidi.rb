d27d7474500586bfdfbef20ffb55b499c2bf5b92
Geert Bevin <gbevin@uwyn.com>
2024-10-15 23:20:58 -0400

    receivemidi 1.4.4

--- Formula/receivemidi.rb
+++ Formula/receivemidi.rb
@@ -1,8 +1,8 @@
 class Receivemidi < Formula
   desc "Multi-platform command-line tool to monitor and receive MIDI messages"
   homepage "https://github.com/gbevin/ReceiveMIDI"
-  url "https://github.com/gbevin/ReceiveMIDI/archive/1.4.3.tar.gz"
-  sha256 "d51c8c5cf378abce048ecabf6e4dc68ba735d75bdc78f3150b3e6117ee2df906"
+  url "https://github.com/gbevin/ReceiveMIDI/archive/1.4.4.tar.gz"
+  sha256 "8e89b22c599bfeafd76b598579d9c3b3a6544adc72267d29ddcd315db3d34c3b"
 
   head "https://github.com/gbevin/ReceiveMIDI.git"
 
