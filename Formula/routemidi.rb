fe6d7d5b647bb1df571c39195280c230e5f48696
Geert Bevin <gbevin@uwyn.com>
2026-07-07 16:51:11 -0400

    Updated RouteMIDI to 0.9.6

--- Formula/routemidi.rb
+++ Formula/routemidi.rb
@@ -1,8 +1,8 @@
 class Routemidi < Formula
   desc "Multi-platform command-line tool to route and transform MIDI messages"
   homepage "https://github.com/gbevin/RouteMIDI"
-  url "https://github.com/gbevin/RouteMIDI/archive/refs/tags/0.9.5.tar.gz"
-  sha256 "414c3b54a31caf3670bc7313a8a516271c2a10fe9738fdfa2c54880f7d94278e"
+  url "https://github.com/gbevin/RouteMIDI/archive/refs/tags/0.9.6.tar.gz"
+  sha256 "b2300587a463407d7ce8668fbce52959e2c1f90a4324684e0b1092439fa24c28"
 
   head "https://github.com/gbevin/RouteMIDI.git"
 
