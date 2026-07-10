f3dc8f5daed4edea7604eca1e94509e96bc360d0
Geert Bevin <gbevin@uwyn.com>
2026-07-10 13:53:50 -0400

    Updated sendmidi to 1.4.0

--- Formula/sendmidi.rb
+++ Formula/sendmidi.rb
@@ -1,8 +1,8 @@
 class Sendmidi < Formula
   desc "Multi-platform command-line tool to send out MIDI messages"
   homepage "https://github.com/gbevin/SendMIDI"
-  url "https://github.com/gbevin/SendMIDI/archive/refs/tags/1.3.1.tar.gz"
-  sha256 "64faf668e3e670fb02a12ffb84172ee3fb70ef1b56804818fe0577eb66d3ec9c"
+  url "https://github.com/gbevin/SendMIDI/archive/refs/tags/1.4.0.tar.gz"
+  sha256 "25cb9f3f1ddd4a7a481eee6d3fc5f34420397710c78bf3c0061f07cd996e946a"
 
   head "https://github.com/gbevin/SendMIDI.git"
 
